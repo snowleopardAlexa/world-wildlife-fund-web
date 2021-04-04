ef8efca6180958e3fb904b1b1f36aa2066b077cf
Alexa Slomski <alexaslomski@Alexas-MBP.home>
2021-04-03 20:12:02 -0400

    added inline styles as the only option for now to override the react bootstrap styles

--- src/components/jumbotron/Header.js
+++ src/components/jumbotron/Header.js
@@ -2,31 +2,48 @@ import React from "react";
 import '../../css/card-intro.css';
 import { Card, Button } from 'react-bootstrap';
 
+const btnAdopt = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
 
 function Header() {
-    return (
-        <>
+return (
+<>
 <div class="header box effect7">
- <Card >
+ <Card fluid>
   <Card.Body>
     <Card.Title class="title">World Wildlife Fund for Nature</Card.Title>
-    <Card.Subtitle class="subtitle">Conservation efforts</Card.Subtitle>
+    <Card.Subtitle class="subtitle">Conservation efforts.</Card.Subtitle>
     <Card.Text class="desc">
-      with love and dedication to the nature
+      with love and dedication to the nature.
     </Card.Text>
-    <Card.Link href="#">
-      <Button variant="primary">DONATE</Button>
-    </Card.Link>
-    <Card.Link href="#">
-      <Button variant="primary">ADOPT</Button>
-    </Card.Link>
+    <div class="button">
+     <Button className="btn-donate mx-auto d-block" size="lg" block 
+      style={{backgroundColor: 'black',
+              border: 'none', 
+              borderRadius: '60px', 
+              padding: '1em 3em', 
+              border: '0'}}
+            >DONATE</Button>
+    </div>
+    <div class="button">
+     <Button className="btn-adopt mx-auto d-block" size="lg" block 
+     style={{backgroundColor: 'black', 
+             border: 'none', 
+             borderRadius: '60px', 
+             padding: '1em 3em', 
+             border: '0',
+             marginTop: '20px'}}
+            >ADOPT</Button>
+    </div>
   </Card.Body>
 </Card>
-
-
-        </div>
-        </>
-    );
+</div>
+</>
+  );
 }
 
 export default Header;
